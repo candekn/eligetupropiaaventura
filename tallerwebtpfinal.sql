643b589f3cdf02a22c17847a3d1b59fa78b52398
DianaAguayo <31738298+DianaAguayoUnlam@users.noreply.github.com>
2019-06-10 00:21:20 -0300

    inserts en Mysql de las primeras dos rutas y resp

--- tallerwebtpfinal.sql
+++ tallerwebtpfinal.sql
@@ -1,29 +1,36 @@
-drop database if exists tallerwebtpfinal;
-create database tallerwebtpfinal;
+/*Ejecutar en Mysql
+ 
+ Antes, hay que modificar el tamaño del varchar(650) del texto de la ruta
+ en el Mysql, si no no va a guardar los textos largos
+ 
+// En la tabla ruta:
+
+insert into ruta(opcion1, opcion2, texto)
+values("Humanidades","Ingenieria","Luego de terminar la secundaria, decidí ingresar a la Universidad. Aprobé el curso de ingreso en el primer intento.
+Después de unas pequeñas vacaciones, ha llegado el primer día de mi carrera universitaria.
+Decidí elegir Desarrollo web, ya que siempre me ha gustado la informática.
+La primera materia es matemáticas, en el aula 232. Según el mapa de la página web, puedo llegar hasta allí desde el departamento de Humanidades o el de Ingeniería.
+¿Por cuál debería ir?");
+
+
+
+insert into ruta(opcion1, opcion2, texto)
+values ("No preguntar","Preguntarle","¡Primer día de clases y ya estoy llegando tarde!
+
+Bueno, este debe ser el aula. Me peino un poco y entro.
+
+- Jugador: Buenos días profesor.
+
+- Profesor: Buenas noches, en el fondo hay una silla desocupada.
+
+- Profesor: “...El segundo cálculo para resolver este ejercicio es...”
+
+No entiendo qué está explicando. ¿Debería preguntarle a mi compañero?");
+
+
+// En la tabla respuesta:
+
 use tallerwebtpfinal;
-/*create table Jugador
-(	id long auto_increment,
-	nombre varchar(60),
-    primary key(id)
-);
-
-create table Preguntas
-(	id long auto_increment,
-	codPregunta varchar(30),
-    texto varchar(500),
-    primary key(id)
-);
-
-create table Respuestas
-(	id long auto_increment,
-	codRespuesta varchar(30),
-    texto varchar(300),
-    primary key(id)
-);
-create table Rutas
-(	id long auto_increment,
-	codRuta varchar(30),
-    texto varchar(500),
-    primary key(id)
-);*/
-
+Insert into respuesta(rendimiento,social,estres,dinero,respuesta,ruta_id) values (10,10,10,10,"Jugar",1);
+Insert into respuesta(rendimiento,social,estres,dinero,respuesta,ruta_id) values (4,-3,-2,1,"Humanidades",2);
+ */
