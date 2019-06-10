5720c36aced7aa60bfed89fabc3c752ae6139d7e
DianaAguayo <31738298+DianaAguayoUnlam@users.noreply.github.com>
2019-06-10 00:30:41 -0300

    metodos para ruteo

---
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioJuego.java
@@ -0,0 +1,21 @@
+package ar.edu.unlam.tallerweb1.servicios;
+
+import java.util.List;
+import java.util.Objects;
+
+import ar.edu.unlam.tallerweb1.modelo.Jugador;
+import ar.edu.unlam.tallerweb1.modelo.Opcion;
+import ar.edu.unlam.tallerweb1.modelo.Respuesta;
+import ar.edu.unlam.tallerweb1.modelo.Ruta;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+
+
+public interface ServicioJuego {
+	
+	
+	 void guardarJugador(Jugador jugador);
+	 
+     Respuesta consultarCodigoRuta(String rta);
+	 
+	 
+}
