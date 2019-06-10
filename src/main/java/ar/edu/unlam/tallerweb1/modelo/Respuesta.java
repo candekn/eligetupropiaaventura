5c85e71a60e0d7f0f4e7a3b82d579b7bab4da664
IvanNicolasKarlen <50282761+IvanNicolasKarlen@users.noreply.github.com>
2019-06-09 22:06:34 -0300

    Corrijo relacion @ManyToMany

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Respuesta.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Respuesta.java
@@ -1,66 +1,91 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
 import java.util.ArrayList;
-
-
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-
-
+import javax.persistence.OneToOne;
 @Entity
 public class Respuesta {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String rta;
-	@OneToMany
-	@Cascade (value= CascadeType.ALL)
-	private ArrayList<Opcion> opciones;
-	 @ManyToMany(mappedBy = "respuestas")
-	    private ArrayList<Jugador> jugadores;
+	
+	private String respuesta;
+	private Integer rendimiento;
+	private Integer estres;
+	private Integer social;
+	private Integer dinero;
+	
+	@OneToOne
+	private Opcion opcion;
+	
+	@ManyToMany(mappedBy = "respuestas")
+	private List<Jugador> jugadores = new ArrayList<>();
+	
 	@ManyToOne
 	private Ruta ruta;
-	 
-	 
-	 public Ruta getRuta() {
-		return ruta;
-	}
-	public void setRuta(Ruta ruta) {
-		this.ruta = ruta;
-	}
-	public ArrayList<Opcion> getOpciones() {
-		return opciones;
-	}
-	public void setOpciones(ArrayList<Opcion> opciones) {
-		this.opciones = opciones;
-	}
+	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getRta() {
-		return rta;
+	public String getRespuesta() {
+		return respuesta;
 	}
-	public void setRta(String rta) {
-		this.rta = rta;
+	public void setRespuesta(String respuesta) {
+		this.respuesta = respuesta;
 	}
-	public ArrayList<Jugador> getJugadores() {
+	public Opcion getOpcion() {
+		return opcion;
+	}
+	public void setOpcion(Opcion opcion) {
+		this.opcion = opcion;
+	}
+	public Ruta getRuta() {
+		return ruta;
+	}
+	public void setRuta(Ruta ruta) {
+		this.ruta = ruta;
+	}
+	
+	public List<Jugador> getJugadores() {
 		return jugadores;
 	}
-	public void setJugadores(ArrayList<Jugador> jugadores) {
+	public void setJugadores(List<Jugador> jugadores) {
 		this.jugadores = jugadores;
 	}
-	 
-	 
+	public Integer getRendimiento() {
+		return rendimiento;
+	}
+	public void setRendimiento(Integer rendimiento) {
+		this.rendimiento = rendimiento;
+	}
+	public Integer getEstres() {
+		return estres;
+	}
+	public void setEstres(Integer estres) {
+		this.estres = estres;
+	}
+	public Integer getSocial() {
+		return social;
+	}
+	public void setSocial(Integer social) {
+		this.social = social;
+	}
+	public Integer getDinero() {
+		return dinero;
+	}
+	public void setDinero(Integer dinero) {
+		this.dinero = dinero;
+	}
 }
+
