fce2a1d892b00e7ed83d3ea06eed922c80f7ddeb
IvanNicolasKarlen <50282761+IvanNicolasKarlen@users.noreply.github.com>
2019-06-09 22:04:14 -0300

    Corrijo relacion @ManyToMany

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Jugador.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Jugador.java
@@ -1,6 +1,7 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
@@ -25,65 +26,96 @@ public class Jugador {
 	private Integer estres;
 	private Integer social;
 	private Integer dinero;
+	private Integer puntaje;
+	private String ultimaRespuesta;
+	
 	@OneToOne
-	@Cascade (value = CascadeType.ALL)
 	private Usuario usuario;
-	  @ManyToMany
+	  
+	@ManyToMany()
 	  @JoinTable(name="JugadorRespuesta", 
 	  joinColumns={@JoinColumn(name="IdJugador")}, 
 	  inverseJoinColumns={@JoinColumn(name="IdRespuesta")})
-		    private ArrayList<Respuesta> respuestas;
-	  
-	  
-	  
-	public Usuario getUsuario() {
-		return usuario;
-	}
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
+		    private List<Respuesta> respuestas;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public Integer getRendimiento() {
 		return rendimiento;
 	}
+
 	public void setRendimiento(Integer rendimiento) {
 		this.rendimiento = rendimiento;
 	}
+
 	public Integer getEstres() {
 		return estres;
 	}
+
 	public void setEstres(Integer estres) {
 		this.estres = estres;
 	}
+
 	public Integer getSocial() {
 		return social;
 	}
+
 	public void setSocial(Integer social) {
 		this.social = social;
 	}
+
 	public Integer getDinero() {
 		return dinero;
 	}
+
 	public void setDinero(Integer dinero) {
 		this.dinero = dinero;
 	}
-	public ArrayList<Respuesta> getRespuestas() {
+
+	public Integer getPuntaje() {
+		return puntaje;
+	}
+
+	public void setPuntaje(Integer puntaje) {
+		this.puntaje = puntaje;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public List<Respuesta> getRespuestas() {
 		return respuestas;
 	}
-	public void setRespuestas(ArrayList<Respuesta> respuestas) {
+
+	public void setRespuestas(List<Respuesta> respuestas) {
 		this.respuestas = respuestas;
 	}
-	  
-	  
+
+	public String getUltimaRespuesta() {
+		return ultimaRespuesta;
+	}
+
+	public void setUltimaRespuesta(String ultimaRespuesta) {
+		this.ultimaRespuesta = ultimaRespuesta;
+	}
+  	  
 }
