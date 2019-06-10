2e6a7ae9656e968855f033d6cf6a08400bad2518
DianaAguayo <31738298+DianaAguayoUnlam@users.noreply.github.com>
2019-06-10 00:32:47 -0300

    Implementacion del servicioJuego

---
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioJuegoImpl.java
@@ -0,0 +1,37 @@
+package ar.edu.unlam.tallerweb1.servicios;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.unlam.tallerweb1.dao.JuegoDao;
+import ar.edu.unlam.tallerweb1.dao.UsuarioDao;
+import ar.edu.unlam.tallerweb1.modelo.Jugador;
+import ar.edu.unlam.tallerweb1.modelo.Opcion;
+import ar.edu.unlam.tallerweb1.modelo.Respuesta;
+import ar.edu.unlam.tallerweb1.modelo.Ruta;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+
+@Service("ServicioJuego")
+@Transactional
+public class ServicioJuegoImpl implements ServicioJuego {
+
+	@Inject
+	private JuegoDao juegoDao;
+	
+	@Override
+	public void guardarJugador(Jugador jugador){
+		juegoDao.guardarJugador(jugador);
+	}
+	
+	@Override
+	public Respuesta consultarCodigoRuta(String rta){
+		return juegoDao.consultarCodigoRuta(rta);
+	}
+	
+	
+}
