cb90d1e94a1c13e9ad80096f13e05cea51928747
DianaAguayo <31738298+DianaAguayoUnlam@users.noreply.github.com>
2019-06-10 00:37:16 -0300

    Interfaz dao

---
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/JuegoDao.java
@@ -0,0 +1,16 @@
+package ar.edu.unlam.tallerweb1.dao;
+
+import java.util.List;
+
+import ar.edu.unlam.tallerweb1.modelo.Jugador;
+import ar.edu.unlam.tallerweb1.modelo.Opcion;
+import ar.edu.unlam.tallerweb1.modelo.Respuesta;
+import ar.edu.unlam.tallerweb1.modelo.Ruta;
+
+public interface JuegoDao {
+	
+	void guardarJugador(Jugador jugador);
+	
+	public Respuesta consultarCodigoRuta(String rta);
+	
+}
