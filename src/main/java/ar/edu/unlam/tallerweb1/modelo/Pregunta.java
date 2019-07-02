f7412159724207c285690b644cbb77cc329e2611
DianaAguayo <31738298+DianaAguayoUnlam@users.noreply.github.com>
2019-07-02 14:39:37 -0300

    relacion pregunta con imagenes

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Pregunta.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Pregunta.java
@@ -1,11 +1,18 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Pregunta {
@@ -15,10 +22,23 @@ public class Pregunta {
 	@Column( name="textoDeLaPregunta", length=650)
 	private String textoDeLaPregunta;
 
-	private String imagen;
+
+	private String estado;
+
+
+		@ManyToMany()
+	  @JoinTable(name="TablaPreguntaPersonaje", 
+	  joinColumns={@JoinColumn(name="IdPregunta")}, 
+	  inverseJoinColumns={@JoinColumn(name="IdImagenPersonaje")})
+		    private List<ImagenPersonaje> personajes;
+
+	
 	
 
-
+	
+	@ManyToOne
+	private ImagenFondo imagenfondo;
+	
 	public Long getId() {
 		return id;
 	}
@@ -31,14 +51,25 @@ public class Pregunta {
 	public void setTextoDeLaPregunta(String textoDeLaPregunta) {
 		this.textoDeLaPregunta = textoDeLaPregunta;
 	}
-	public String getImagen() {
-		return imagen;
+	
+	public String getEstado() {
+		return estado;
 	}
-	public void setImagen(String imagen) {
-		this.imagen = imagen;
+	public void setEstado(String estado) {
+		this.estado = estado;
 	}
 	
-	
+	public ImagenFondo getImagenfondo() {
+		return imagenfondo;
+	}
+	public void setImagenfondo(ImagenFondo imagenfondo) {
+		this.imagenfondo = imagenfondo;
+	}
+	public List<ImagenPersonaje> getPersonajes() {
+		return personajes;
+	}
+	public void setPersonajes(List<ImagenPersonaje> personajes) {
+		this.personajes = personajes;
+	}
 
-	
 }
