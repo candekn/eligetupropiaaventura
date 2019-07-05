d03c1a2c3891d693885fcb2c01ae87efe5034380
DianaAguayo <31738298+DianaAguayoUnlam@users.noreply.github.com>
2019-07-05 14:42:33 -0300

    buscar Respuestas Anteriores dao

--- src/main/java/ar/edu/unlam/tallerweb1/dao/JuegoDao.java
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/JuegoDao.java
@@ -36,4 +36,6 @@ public interface JuegoDao {
 
 	List<ImagenPersonaje> buscarImagenesDePersonajes(Pregunta siguientePregunta);
 	
+	List<TablaJugadorRespuesta> buscarRespuestasAnteriores(Jugador mij);
+	
 }
